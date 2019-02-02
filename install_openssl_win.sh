363915751733a829c9af3d48cf0d7a8825474902
okrplay <32576280+okrplay@users.noreply.github.com>
2019-02-02 16:19:47 +0100

    Forgot to change Chocolatey package
    Signed-off-by: okrplay <32576280+okrplay@users.noreply.github.com>

--- install_openssl_win.sh
+++ install_openssl_win.sh
@@ -1,4 +1,4 @@
 if [ "$TRAVIS_OS_NAME" == "windows" ];
 then
-    choco install openssl.light --params "/InstallDir:C:\Users\travis\libressl_dev"
+    choco install libressl --params "/InstallDir:C:\Users\travis\libressl_dev"
 fi
