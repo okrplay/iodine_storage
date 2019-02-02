b81715752f695eb79875444d1674c9c119d72b01
okrplay <32576280+okrplay@users.noreply.github.com>
2019-02-02 16:07:26 +0100

    Back to Chocolatey, now using LibreSSL on Windows
    Signed-off-by: okrplay <32576280+okrplay@users.noreply.github.com>

--- install_openssl_win.sh
+++ install_openssl_win.sh
@@ -1,5 +1,4 @@
 if [ "$TRAVIS_OS_NAME" == "windows" ];
 then
-    curl -O https://slproweb.com/download/Win64OpenSSL-1_1_1a.exe
-    ./Win64OpenSSL-1_1_1a.exe /SILENT /VERYSILENT /SP- /DIR="C:\Users\travis\openssl_dev"
+    choco install openssl.light --params "/InstallDir:C:\Users\travis\libressl_dev"
 fi
