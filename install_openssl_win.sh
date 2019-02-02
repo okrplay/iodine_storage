32144c40602addfa0b917953c7ae7d149f2d2e7a
okrplay <32576280+okrplay@users.noreply.github.com>
2019-02-02 15:44:06 +0100

    OpenSSL installation is now manual
    Signed-off-by: okrplay <32576280+okrplay@users.noreply.github.com>

--- install_openssl_win.sh
+++ install_openssl_win.sh
@@ -1,4 +1,5 @@
 if [ "$TRAVIS_OS_NAME" == "windows" ];
 then
-    choco install openssl.light --params "/InstallDir:C:\Users\travis\openssl_lib"
+    curl -O https://slproweb.com/download/Win64OpenSSL-1_1_1a.exe
+    ./Win64OpenSSL-1_1_1a.exe /SILENT /VERYSILENT /SP- /DIR="C:\Users\travis\openssl_dev"
 fi
