5ab927bdb6ff5386ea433881a8a71aecc53941e1
okrplay <32576280+okrplay@users.noreply.github.com>
2019-02-02 14:22:54 +0100

    Fix Travis CI Windows OpenSSL build error #4
    Signed-off-by: okrplay <32576280+okrplay@users.noreply.github.com>

--- install_openssl_win.sh
+++ install_openssl_win.sh
@@ -1,4 +1,5 @@
-if [ "$TRAVIS_OS_NAME" == "windows" ] then
+if [ "$TRAVIS_OS_NAME" == "windows" ];
+then
     choco install openssl.light --params "/InstallDir:C:\Users\travis\openssl_lib"
     exec shell
 fi
