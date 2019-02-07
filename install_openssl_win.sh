e33dc05dcecb37d61a9751b87c9838b452e1d9e7
okrplay <32576280+okrplay@users.noreply.github.com>
2019-02-07 21:12:03 +0100

    Powershell command should now run with powershell
    Signed-off-by: okrplay <32576280+okrplay@users.noreply.github.com>

--- install_openssl_win.sh
+++ install_openssl_win.sh
@@ -1,5 +1,4 @@
 if [ "$TRAVIS_OS_NAME" == "windows" ];
 then
     curl -O https://slproweb.com/download/Win64OpenSSL-1_1_1a.exe
-    powershell ./Win64OpenSSL-1_1_1a.exe /SILENT /VERYSILENT /SP- /DIR="C:\Users\travis\openssl_dev"
 fi
