2dc0b1912bce9096a7e61f592a577f3a75159779
okrplay <32576280+okrplay@users.noreply.github.com>
2019-02-07 20:52:35 +0100

    Powershell command moved to bash file
    Signed-off-by: okrplay <32576280+okrplay@users.noreply.github.com>

--- install_openssl_win.sh
+++ install_openssl_win.sh
@@ -1,4 +1,5 @@
 if [ "$TRAVIS_OS_NAME" == "windows" ];
 then
     curl -O https://slproweb.com/download/Win64OpenSSL-1_1_1a.exe
+    powershell ./Win64OpenSSL-1_1_1a.exe /SILENT /VERYSILENT /SP- /DIR="C:\Users\travis\openssl_dev"
 fi
