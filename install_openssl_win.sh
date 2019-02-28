656f76094c8789b2a7505abb4bb29400daee39fd
okrplay <32576280+okrplay@users.noreply.github.com>
2019-02-28 16:29:12 +0100

    Updated Travis Windows OpenSSL installation
    Signed-off-by: okrplay <32576280+okrplay@users.noreply.github.com>

--- install_openssl_win.sh
+++ install_openssl_win.sh
@@ -1,5 +1,5 @@
 if [ "$TRAVIS_OS_NAME" == "windows" ];
 then
-    curl -O https://slproweb.com/download/Win64OpenSSL-1_1_1a.exe
-    PowerShell -Command './Win64OpenSSL-1_1_1a.exe /SILENT /VERYSILENT /SP- /DIR=C:\Users\travis\openssl_dev'
+    curl -O https://slproweb.com/download/Win64OpenSSL-1_1_1b.exe
+    PowerShell -Command './Win64OpenSSL-1_1_1b.exe /SILENT /VERYSILENT /SP- /DIR=C:\Users\travis\openssl_dev'
 fi
